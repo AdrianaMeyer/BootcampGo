6b8d60a88a542bd0df9b47fec1513145f2504865
AdrianaMeyer <adriana.meyer@mercadolivre.com>
2023-05-29 09:53:31 -0300

    Adicionando o exercicio da tarde

---
+++ Aula03/Tarde/exercicio01_registroEstaudantes.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Aluno struct {
+	nome string 
+	sobrenome string 
+	rg string 
+	dataAdmissao string 
+
+}
+
+func detalhes(aluno Aluno) {
+	fmt.Println("--------- Dados do Aluno ------- \t")
+	fmt.Println("Nome:", aluno.nome)
+	fmt.Println("Sobrenome:", aluno.sobrenome)  
+	fmt.Println("RG:", aluno.rg)  
+	fmt.Println("Data de Admissão:", aluno.dataAdmissao)       
+}
+
+
+func main() {
+	aluno1 := Aluno {
+		nome: "João",
+		sobrenome: "Silva",
+		rg: "11.234.567-8",
+		dataAdmissao: "12/04/2010",
+	}
+
+	aluno2 := Aluno {
+		nome: "Maria",
+		sobrenome: "Ferreira",
+		rg: "12.134.567-9",
+		dataAdmissao: "01/02/2012",
+	}
+
+	detalhes(aluno1)
+	detalhes(aluno2)
+}
