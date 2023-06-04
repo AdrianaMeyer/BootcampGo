c74a54fa5fdc20c237a4a51cc30ffdf7b909b831
AdrianaMeyer <adriana.meyer@mercadolivre.com>
2023-06-04 20:22:34 -0300

    Ajustando exercicios 01 e 02(manhã)

--- Aula04 - Go Bases III/Manha/exercicio01_GuardarArquivo.go
+++ Aula04 - Go Bases III/Manha/exercicio01_GuardarArquivo.go
@@ -3,36 +3,49 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 type Produto struct {
-	id int
-	preco float64
-	quantidade int
+	Id int
+	Preco float64
+	Quantidade int
+}
+
+func NovoProduto(id int, preco float64, quant int) Produto {
+	novoProduto := Produto {
+		Id: id,
+		Preco: preco,
+		Quantidade: quant,
+	}
+	return novoProduto
 }
 
 func main() {
 
-	produto1 := Produto {
-		id: 12464,
-		preco: 12.50,
-		quantidade: 45,
+	produto1 := NovoProduto(101, 12.50, 15)
+	produto2 := NovoProduto(102, 150.90, 23)
+	produto3 := NovoProduto(103, 204.99, 19)
+
+	produtos := []Produto {produto1, produto2, produto3}
+
+	var linhas []string
+
+	linhas = append(linhas, "ID;Preco;Quantidade;")
+
+	for _, produto := range produtos {
+		linha := fmt.Sprintf("%d;%.2f;%d;", produto.Id, produto.Preco, produto.Quantidade)
+		linhas = append(linhas, linha)
 	}
 
-	/*produto2 := Produto {
-		id: 23547,
-		preco: 50.49,
-		quantidade: 130,
-	}
-	*/
+	conteudoCsv := []byte(strings.Join(linhas, "\n"))
 
-	infoProduto := fmt.Sprintf("ID;Preco;Quantidade;%d;%.2f;%d", produto1.id, produto1.preco, produto1.quantidade)
-	
-	conteudo := []byte(infoProduto)
-	erro := os.WriteFile("./relacaoProdutos.csv", conteudo, 0644)
+	err := os.WriteFile("./relacaoProdutos.csv", conteudoCsv, 0644)
 
-	if erro != nil {
-		fmt.Println("Erro ao escrever as informacoes no arquivo", erro)
+	if err != nil {
+		fmt.Println("Erro ao escrever as informacoes no arquivo", err)
 	}
+
+	fmt.Println("Dados salvos com sucesso")
 
 }
--- Aula04 - Go Bases III/Manha/exercicio02_LerArquivo.go
+++ Aula04 - Go Bases III/Manha/exercicio02_LerArquivo.go
@@ -1,24 +1,67 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
+	"strconv"
+	"log"
 )
 
 func main() {
 
-	arquivo, erro := os.ReadFile("./relacaoProdutos.csv")
+	var dadosProduto []string
 
-	if erro != nil {
-		fmt.Println("Houve erro ao ler o arquivo", erro)
+	arquivo, err := os.Open("./relacaoProdutos.csv")
+
+	if err != nil {
+		fmt.Println("Houve erro ao abrir o arquivo", err)
+		arquivo.Close()
 	}
 
-	dadosProduto := strings.Slipt(arquivo, ";")
+	leitor := bufio.NewReader(arquivo)
 
-	for _, dado := range dadosProduto {
-		fmt.Println(dado)
+	for {	
+		dado, err := leitor.ReadString(';')
+		dado = strings.Trim(dado, ";")
+
+		if err == io.EOF {
+			break
+		}
+
+		dadosProduto = append(dadosProduto, dado)
+		
+	}
+	arquivo.Close()
+
+	var total float64 = 0.0
+
+	//CABECALHO
+	for i := 0; i < 1; i++ {
+		fmt.Printf("%s\t %s\t %s\t", dadosProduto[i], dadosProduto[i+1], dadosProduto[i+2])
+
 	}
 
-	//EXERCICIO EM AJUSTES
+	//CORPO
+	for j := 3; j < len(dadosProduto); {
+
+		preco, err := strconv.ParseFloat(dadosProduto[j+1], 64)
+		quant, err := strconv.ParseFloat(dadosProduto[j+2], 64)
+
+		if err != nil {
+		log.Fatal("Erro ao converter a string para float64:", err)
+		}
+
+		total = total + (preco * quant)
+
+		fmt.Printf("%s\t %s\t %s\t", dadosProduto[j], dadosProduto[j+1], dadosProduto[j+2])
+		j += 3
+
+	}
+
+	fmt.Printf("\n%s\t %.2f\t", "TOTAL: ", total)
+	
 }
+
