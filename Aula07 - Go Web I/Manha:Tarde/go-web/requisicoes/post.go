7508e99d9be208a68c55886c98af7b4db3201a31
AdrianaMeyer <adriana.meyer@mercadolivre.com>
2023-06-07 11:27:53 -0300

    Adicionando a validacao do token no envio da requsicao POST

--- Aula07 - Go Web I/Manha:Tarde/go-web/requisicoes/post.go
+++ Aula07 - Go Web I/Manha:Tarde/go-web/requisicoes/post.go
@@ -5,20 +5,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var lastID int
-
 func SaveUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
+        var lastID int
 		var req Users
-		err := c.ShouldBindJSON(&req);
+        token := c.GetHeader("token")
+		
+		if token != "123456" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"Error": "Token inválido - você não tem permissao para fazer esta solicitação",
+			})
+			return
+		}
 
+        if len(users) == 0 {
+            lastID = 0
+        } else {
+            lastUser := len(users) -1
+            lastID =  users[lastUser].Id     
+        }
+
+        err := c.ShouldBindJSON(&req);
 		if  err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error": err.Error(),
 			})
 			return
 		}
-		
+
 		lastID++
 		req.Id = lastID
 		users = append(users, req)
